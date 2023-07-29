068cf85018d7f925c3e4cc44ee549583c6746c30
nader <nhashemi6@gmail.com>
2023-07-29 17:10:22 +0330

    Add project files.

---
+++ MoneyComma.Project1/js/comma.js
@@ -0,0 +1,15 @@
+function moneyCommaSep(ctrl) {
+            var separator = ",";
+    var int = ctrl.value.replace(new RegExp(separator, "g"), "");
+    var regexp = new RegExp("\\B(\\d{3})(" + separator + "|$)");
+    do {
+        int = int.replace(regexp, separator + "$1");
+            }
+            while (int.search(regexp) >= 0)
+    ctrl.value = int;
+        }
+
+    function removeComma(str) {
+            var separator = ",";
+            return str.replace(new RegExp(separator, "g"), "");
+        }
